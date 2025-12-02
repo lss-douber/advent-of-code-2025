504860d8724cb402e1904a7b43d25648b203f810
Lucas Souza Santos <lss.souzasantos@outlook.com>
2025-12-02 17:33:27 -0300

    finished day1 part one

--- src/day1.ts
+++ src/day1.ts
@@ -2,18 +2,18 @@ import { readFileFromPath } from "./utils/functions";
 
 type TDialRotation = `${"L" | "R"}${number}`;
 
-const input: TDialRotation[] = (
-  await readFileFromPath("./src/inputs/day1.txt")
-).split("\r\n") as TDialRotation[];
+const input: TDialRotation[] = (await readFileFromPath("./src/inputs/day1.txt"))
+  .replaceAll("\r\n", "\n")
+  .split("\n") as TDialRotation[];
 const dial = Array.from({ length: 100 }).map((_, idx) => idx);
 
 class Dialer {
   public arr: number[];
   public position: number;
 
-  public constructor(arr: number[], position?: number) {
+  public constructor(arr: number[], position: number) {
     this.arr = arr;
-    this.position = typeof position === "number" ? position : 50;
+    this.position = position;
   }
 
   dial(rotation: TDialRotation) {
@@ -21,37 +21,27 @@ class Dialer {
       ? -+rotation.slice(1)
       : +rotation.slice(1);
 
-    console.log({ rotationNumber });
-    console.log({ position: this.position });
-
     const diff = this.position + rotationNumber;
 
-    // if the diff is within the arr range
-    if (diff >= 0 && diff < this.arr.length) {
-      return new Dialer(this.arr, this.arr[diff]);
-    }
-
     if (diff < 0) {
-      return new Dialer(this.arr, this.arr[this.arr.length % diff]);
+      return new Dialer(
+        this.arr,
+        this.arr[
+          (this.arr.length + (diff % this.arr.length)) % this.arr.length
+        ]!
+      );
     }
 
-    if (diff > this.arr.length) {
-      return new Dialer(this.arr, this.arr[diff % this.arr.length]);
-    }
-
-    return new Dialer(this.arr, this.arr[diff]);
+    return new Dialer(this.arr, this.arr[(diff % this.arr.length) as number]!);
   }
 }
 
-const dialer = new Dialer(dial);
+const dialer = new Dialer(dial, 50);
+const zeros: number[] = [];
 
-["R48", "R2", "R29", "L38", "L3"].reduce<Dialer>((acc, curr, idx) => {
-  // console.log(acc.position);
-  // console.log({idx});
-
-  console.log({ curr });
-
-  console.log(acc.position);
-
+input.reduce<Dialer>((acc, curr, idx) => {
+  acc.position === 0 && zeros.push(acc.position);
   return acc.dial(curr);
 }, dialer);
+
+console.log(zeros.length);
